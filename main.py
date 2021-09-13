cf8e453e9c2bd524e583664319de62c22e270993
KazumaKobayash <kazuma.kobayashi.3t@gmail.com>
2021-09-13 16:19:45 +0900

    modified main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 #!/bin/python3
 
 with open('data/sample1.txt', 'r') as f:
-    print(f.read())
+    for idx, line in enumerate(f):
+        line = line.strip("\n") # 改行記号削除
+        print(f"{idx}:{line}")

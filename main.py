823781b2c845fb7cdc00b41535bfb66a5e280d9e
kabukiage3 <haruki050648@gmail.com>
2021-09-14 14:21:04 +0900

    仕様７を実装

--- main.py
+++ main.py
@@ -2,16 +2,18 @@
 import sys
 file_path = sys.argv[1]
 
-with open(file_path, 'r') as f:
+with open(file_path, 'r', encoding="utf-8") as f:
     lines = f.readlines()
     
     if len(sys.argv) > 2: # 表示したい行目を指定している場合。
         for i in range(2,len(sys.argv)):
             idx = int(sys.argv[i]) # 表示したい行目。 e.g. 2
             line = lines[idx-1].strip("\n")
-            print(f"{idx}:{line}")
+            word, word_info = line.split()
+            print(f"{idx}: {word} {word_info}")
 
     elif len(sys.argv) == 2: # 表示したい行目を指定していない場合。
         for idx, line in enumerate(lines):
             line = line.strip("\n")
-            print(f"{idx+1}:{line}")
+            word, word_info = line.split()
+            print(f"{idx+1}: {word} {word_info}")

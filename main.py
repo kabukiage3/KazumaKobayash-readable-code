f6296147e4c84dacbb46267822d4aa7485322c36
KazumaKobayash <kazuma.kobayashi.3t@gmail.com>
2021-09-14 13:38:43 +0900

    Co-authored-by: Bobtenshi <Bobtenshi@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 #!/bin/python3
+import sys
+file_path = sys.argv[1]
 
-with open('data/sample1.txt', 'r') as f:
-    for idx, line in enumerate(f):
-        line = line.strip("\n") # 改行記号削除
-        print(f"{idx}:{line}")
+with open(file_path, 'r') as f:
+    lines = f.readlines()
+    
+    if len(sys.argv) > 2: # 表示したい行目を指定している場合。
+        for i in range(2,len(sys.argv)):
+            idx = int(sys.argv[i]) # 表示したい行目。 e.g. 2
+            line = lines[idx-1].strip("\n")
+            print(f"{idx}:{line}")
+
+    elif len(sys.argv) == 2: # 表示したい行目を指定していない場合。
+        for idx, line in enumerate(lines):
+            line = line.strip("\n")
+            print(f"{idx+1}:{line}")

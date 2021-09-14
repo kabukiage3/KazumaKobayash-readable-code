4a57e5492ca3d3add5dd0dc8ced06fcc4cc8b73a
kabukiage3 <haruki050648@gmail.com>
2021-09-14 14:51:02 +0900

    変数を説明するコメントの追加

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ file_path = sys.argv[1]
 with open(file_path, 'r', encoding="utf-8") as f:
     lines = f.readlines()
     
-    if len(sys.argv) > 2: # 表示したい行目を指定している場合。
+    if len(sys.argv) > 2: # 行数指定あり
         for argidx in range(2,len(sys.argv)):
-            idx = int(sys.argv[argidx]) # 表示したい行目。 e.g. 2
+            idx = int(sys.argv[argidx]) # 指定行数
             line = lines[idx-1].strip("\n")
-            word, word_info = line.split()
-            print(f"{idx}: {word} {word_info}")
+            word, word_info = line.split() # 単語情報, 読みがな
+            print(f"{idx}: {word} {word_info}") 
 
-    elif len(sys.argv) == 2: # 表示したい行目を指定していない場合。
+    elif len(sys.argv) == 2: # 行数指定なし
         for idx, line in enumerate(lines):
             line = line.strip("\n")
-            word, word_info = line.split()
+            word, word_info = line.split() # 単語情報, 読みがな
             print(f"{idx+1}: {word} {word_info}")

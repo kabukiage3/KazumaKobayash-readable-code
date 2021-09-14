93be84dbebe41b9f3fda2e20fd174622d6281739
kabukiage3 <haruki050648@gmail.com>
2021-09-14 14:31:23 +0900

    for文で用いる変数を変更

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ with open(file_path, 'r', encoding="utf-8") as f:
     lines = f.readlines()
     
     if len(sys.argv) > 2: # 表示したい行目を指定している場合。
-        for i in range(2,len(sys.argv)):
-            idx = int(sys.argv[i]) # 表示したい行目。 e.g. 2
+        for argidx in range(2,len(sys.argv)):
+            idx = int(sys.argv[argidx]) # 表示したい行目。 e.g. 2
             line = lines[idx-1].strip("\n")
             word, word_info = line.split()
             print(f"{idx}: {word} {word_info}")
